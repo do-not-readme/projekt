34f6e4a98f84abd873cd52c86d8ef6b24bc0bf44
do-not-readme <dont_readme@icloud.com>
2026-09-26 14:59:05 +0200

    ellenorzo modositasa

--- szkriptek/ellenorzo.sh
+++ szkriptek/ellenorzo.sh
@@ -4,6 +4,7 @@ shopt -s inherit_errexit
 
 
 hibak=0
+
 port_check() {
 	local kod
 	kod="$(curl -s -o /dev/null -w '%{http_code}\n' http://localhost:"$1")"
@@ -24,18 +25,23 @@ szolgaltatas_fut() {
 	fi
 }
 
-ellenoriz() {
-	local nev
-	nev="$1"
-	shift
-	if "$@"; then
-		printf '%s: OK\n' "$nev"
+ellenoriz_szolgaltatas() {
+	if szolgaltatas_fut "$1"; then
+		printf 'A %s szolgaltatas \e[92mFUT\e[0m \n' "$1"
 	else
+		printf 'A %s szolgaltatas \e[91mNEM FUT\e[0m \n' "$1"
 		hibak=$((hibak + 1))
-		printf '%s: HIBA\n' "$nev" >&2
 	fi
 }
 
+ellenoriz_port() {
+	if port_check "$1"; then
+		printf 'A %s port \e[92mNYITVA\e[0m \n' "$1"
+	else
+		printf 'A %s port \e[91mZARVA\e[0m \n' "$1"
+		hibak=$((hibak + 1))
+	fi
+}
 
 ## Szolgaltatas ellenorzes
 
@@ -43,9 +49,9 @@ a_cim='projekt-weboldal-1'
 b_cim='projekt-weboldal2-1'
 c_cim='projekt-weboldal3-1'
 
-ellenoriz "[$a_cim] szolgaltatas ellenorzes" szolgaltatas_fut "$a_cim"
-ellenoriz "[$b_cim] szolgaltatas ellenorzes" szolgaltatas_fut "$b_cim"
-ellenoriz "[$c_cim] szolgaltatas ellenorzese" szolgaltatas_fut "$c_cim"
+ellenoriz_szolgaltatas "$a_cim"
+ellenoriz_szolgaltatas "$b_cim"
+ellenoriz_szolgaltatas "$c_cim"
 
 ## Port ellenorzes
 
@@ -53,9 +59,9 @@ a_port='8080'
 b_port='8081'
 c_port='8082'
 
-ellenoriz "[$a_port] port ellenorzes" port_check "$a_port"
-ellenoriz "[$b_port] port ellenorzes" port_check "$b_port"
-ellenoriz "[$c_port] port ellenorzes" port_check "$c_port"
+ellenoriz_port "$a_port"
+ellenoriz_port "$b_port"
+ellenoriz_port "$c_port"
 
 if [[ "$hibak" -gt 0 ]]; then
 	exit 1
